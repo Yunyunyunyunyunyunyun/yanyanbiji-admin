9fe77486e3f5e6e91731507338c3526554c6c940
wgy <1124551805@qq.com>
2019-01-25 15:43:26 +0800

    mini commit

--- src/pages/user/login.vue
+++ src/pages/user/login.vue
@@ -16,7 +16,7 @@
             <el-input type="password" v-model="form.password" placeholder="请输入账户密码：" class="form-input"></el-input>
           </el-form-item>
           <el-form-item class="login-item">
-            <el-button size="large" icon="check" class="form-submit" @click="submit_form"></el-button>
+            <el-button size="large" icon="check" class="form-submit" @click="submit_form">登录</el-button>
           </el-form-item>
         </el-form>
       </div>
